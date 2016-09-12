8bb2c4a26f4a30289744499726a4f5f642d30707
Cristian Santos Ferreira <cristianferreira_gks@hotmail.com>
2016-09-11 22:56:01 -0300

    [cristian] tabela fabricante pronta

--- ProjetoLoja/src/main/java/br/com/loja/bean/FabricanteBean.java
+++ ProjetoLoja/src/main/java/br/com/loja/bean/FabricanteBean.java
@@ -1,20 +1,24 @@
 package br.com.loja.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.loja.dao.FabricanteDAO;
 import br.com.loja.domain.Fabricante;
 import br.com.loja.util.FacesUtil;
-
 
 @ManagedBean
 @ViewScoped
 public class FabricanteBean {
 
 	private Fabricante fabricante;
+	private List<Fabricante> listFabricante;
+	private List<Fabricante> listFabricanteFiltro;
 
 	public Fabricante getFabricante() {
-		if(fabricante==null){
+		if (fabricante == null) {
 			fabricante = new Fabricante();
 		}
 		return fabricante;
@@ -24,11 +28,48 @@ public class FabricanteBean {
 		this.fabricante = fabricante;
 	}
 
+	public List<Fabricante> getListFabricante() {
+		return listFabricante;
+	}
+
+	public void setListFabricante(List<Fabricante> listFabricante) {
+		this.listFabricante = listFabricante;
+	}
+
+	public List<Fabricante> getListFabricanteFiltro() {
+		return listFabricanteFiltro;
+	}
+
+	public void setListFabricanteFiltro(List<Fabricante> listFabricanteFiltro) {
+		this.listFabricanteFiltro = listFabricanteFiltro;
+	}
+
+	// Metodos
+
 	public void salvar() {
-		
-		FacesUtil.adicionarMsgSucesso(fabricante.toString());
-		FacesUtil.adicionarMsgSucesso("Fabricante salvo com sucesso");
-		// FacesUtil.adicionarMsgErro("Erro ao salvar o fabricante");
+
+		// FacesUtil.adicionarMsgSucesso(fabricante.toString());
+		try {
+			FabricanteDAO fdao = new FabricanteDAO();
+			fdao.salvar(fabricante);
+
+			fabricante = new Fabricante();
+			FacesUtil.adicionarMsgSucesso("Fabricante salvo com sucesso");
+		} catch (RuntimeException ex) {
+			ex.printStackTrace();
+			FacesUtil.adicionarMsgErro("Erro ao tentar salvar o fabricante:" + ex.getMessage());
+		}
+
+	}
+	
+	public void listarFabricante(){
+		try{
+			FabricanteDAO fdao = new FabricanteDAO();
+			listFabricante = fdao.listar();
+		}catch(RuntimeException ex) {
+			ex.printStackTrace();
+			FacesUtil.adicionarMsgErro("Erro ao tentar listar os fabricantes:" + ex.getMessage());
+		}
 	}
 
 }
